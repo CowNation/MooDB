16104afa3809c91172a317abf36eab56ea3c2b76
CowNation <CowNation.cow@gmail.com>
2019-10-30 08:23:25 -0500

    Fixed indentation & cleaned up code

--- Data/MooDatabase.hpp
+++ Data/MooDatabase.hpp
@@ -5,99 +5,101 @@
 #include "CowConfig.hpp"
 
 class Database{
-  private:
-  std::string FindStrBetween(std::string& str, char firstL, char lastL, bool Remove){
-    std::string ret;
-    bool between = false;
-    for (int i = 0; i < str.length(); i++){
-      if (between && str[i] == lastL){
-        break;
-      }
-      
-      if (str[i] == firstL && between == false){
-        between = true;
-      }
-      else if (between){
-        ret += str[i];
-      }
-    }
-    str.erase(0, ret.length() + 2);
-    return ret;
-  }
-  std::vector< std::string > UnseperateCommas(std::string str){
-    std::vector< std::string > ret;
-    std::string temp;
-    for (int i = 0; i < str.length(); i++){
-      if (str[i] != '')
-        temp += str[i];
-      else{
-        ret.push_back(temp);
-        temp = "";
-      }
-    }
-    if (ret.size() == 0)
-      return {};
-    return ret;
-  }
-  CowConfig cfg;
-  public:
-  std::vector< Table > Tables;
+private:
+	std::string FindStrBetween(std::string& str, char firstL, char lastL, bool Remove){
+		std::string ret;
+		bool between = false;
+		for (int i = 0; i < str.length(); i++){
+			if (between && str[i] == lastL)
+				break;
 
-  Database(std::string filePath){
-    cfg.OpenFile(filePath);
-  }
-  void Print(){
-    for (int i = 0; i < Tables.size(); i++)
-      Tables[i].Print();
-  }
-  void InsertTable(Table& table){
-    Tables.push_back(table);
-    table.parentDB = this;
-  }
-  void SaveData(){
-    /*
-      Saved data form:
-      1: {Table1Name{Column1Name,Column2Name}{OneOne,OneTwo}{TwoOne,TwoTwo}}
-    */
-    for (int i = 0; i < Tables.size(); i++){
-      std::string tableLine = "{" + Tables[i].Name + "}{";
-      for (int p = 0; p < Tables[i].Columns.size(); p++){
-        tableLine += Tables[i].Columns[p] + "";
-      }
-      tableLine += "}{";
-      for (int p = 0; p < Tables[i].Data.size(); p++){
-        for (int q = 0; q < Tables[i].Data[p].size(); q++){
-          tableLine += Tables[i].Data[p][q] + "";
-        } // looping horizontally
-        tableLine += "}";
-        if (p != Tables[i].Data.size() - 1)
-          tableLine += "{";
-      }// looping vertically
-      cfg.WriteLine("", tableLine);
-    }
-  }
-  void LoadTables(){
-    std::vector< std::string >Lines = cfg.GetLines();
-    for (int i = 0; i < Lines.size(); i++) {
-      std::string Name = FindStrBetween(Lines[i], '{', '}', true);
-      Table tab(Name, UnseperateCommas(FindStrBetween(Lines[i], '{', '}', true)));
-      while (Lines[i].length() > 0){
-        std::vector< std::string > vc = UnseperateCommas(FindStrBetween(Lines[i], '{', '}', true));
-        tab.Insert(vc);
-      }
+			if (str[i] == firstL && between == false)
+				between = true;
+			else if (between)
+				ret += str[i];
+		}
+		str.erase(0, ret.length() + 2);
+		return ret;
+	}
 
-      Tables.push_back(tab);
-    } // loop through each individual line
-  }
-  Table& GetTable(std::string TableName){
-    for (int i = 0; i < Tables.size(); i++){
-      if (Tables[i].Name == TableName)
-        return Tables[i];
-    }
-    throw "Could not find table '" + TableName + "'";
-  }
+	std::vector< std::string > UnseperateCommas(std::string str){
+		std::vector< std::string > ret;
+		std::string temp;
+		for (int i = 0; i < str.length(); i++){
+			if (str[i] != '')
+				temp += str[i];
+			else{
+				ret.push_back(temp);
+				temp = "";
+			}
+		}
+		return ret;
+	}
+	
+	CowConfig cfg;
+public:
+	std::vector< Table > Tables;
+
+	Database(std::string filePath){
+		cfg.OpenFile(filePath);
+	}
+
+	void Print(){
+		for (int i = 0; i < Tables.size(); i++)
+		Tables[i].Print();
+	}
+
+	void InsertTable(Table& table){
+		Tables.push_back(table);
+		table.parentDB = this;
+	}
+
+	void SaveData(){
+		/*
+		Saved data format:
+		1: {Table1Name{Column1Name,Column2Name}{OneOne,OneTwo}{TwoOne,TwoTwo}}
+		*/
+		for (int i = 0; i < Tables.size(); i++){
+			std::string tableLine = "{" + Tables[i].Name + "}{";
+			for (int p = 0; p < Tables[i].Columns.size(); p++){
+				tableLine += Tables[i].Columns[p] + "";
+			}
+			tableLine += "}{";
+			for (int p = 0; p < Tables[i].Data.size(); p++){
+				for (int q = 0; q < Tables[i].Data[p].size(); q++){
+					tableLine += Tables[i].Data[p][q] + "";
+				} // looping horizontally
+				tableLine += "}";
+				if (p != Tables[i].Data.size() - 1)
+					tableLine += "{";
+			}// looping vertically
+			cfg.WriteLine("", tableLine);
+		}
+	}
+
+	void LoadTables(){
+		std::vector< std::string >Lines = cfg.GetLines();
+		for (int i = 0; i < Lines.size(); i++) {
+			std::string Name = FindStrBetween(Lines[i], '{', '}', true);
+			Table tab(Name, UnseperateCommas(FindStrBetween(Lines[i], '{', '}', true)));
+			while (Lines[i].length() > 0){
+				std::vector< std::string > vc = UnseperateCommas(FindStrBetween(Lines[i], '{', '}', true));
+				tab.Insert(vc);
+			}
+
+			Tables.push_back(tab);
+		} // loop through each individual line
+	}
+
+	Table& GetTable(std::string TableName){
+		for (int i = 0; i < Tables.size(); i++){
+			if (Tables[i].Name == TableName)
+				return Tables[i];
+		}
+		throw "Could not find table '" + TableName + "'";
+	}
 };
 
 void Table::RefreshFile(){
-  parentDB->SaveData();
+	parentDB->SaveData();
 }
--- main.cpp
+++ main.cpp
@@ -5,39 +5,39 @@
 
 template <class T>
 void PrintVector(std::vector< T > vec){
-  std::cout << std::endl;
-  for (int i = 0; i < vec.size(); i++){
-    std::cout << vec[i];
-    if (i != vec.size() - 1)
-      std::cout << ", ";
-  }
+	std::cout << std::endl;
+	for (int i = 0; i < vec.size(); i++){
+		std::cout << vec[i];
+		if (i != vec.size() - 1)
+			std::cout << ", ";
+	}
 }
 
 int main() {
-  Database db("test.txt");
-  Table tab("Patients", {"Name", "Gender", "Age", "DOB", "Height", "Weight"});
-  tab.Insert({"Funanya Radomir", "Female", "3", "6/1/2015", "3 ft 1 in", "29.5 lbs"});
-  tab.Insert({"Reanna Shekhar", "Female", "15", "10/31/2003", "5 ft 6 in", "130 lbs"});
-  db.InsertTable(tab);
+	Database db("test.txt");
+	Table tab("Patients", {"Name", "Gender", "Age", "DOB", "Height", "Weight"});
+	tab.Insert({"Funanya Radomir", "Female", "3", "6/1/2015", "3 ft 1 in", "29.5 lbs"});
+	tab.Insert({"Reanna Shekhar", "Female", "15", "10/31/2003", "5 ft 6 in", "130 lbs"});
+	db.InsertTable(tab);
 	Table hp("Medications", {"Name", "Stock", "RequiresID"});
 	hp.Insert({"Ibuprofin", "423,243", "False"});
 	hp.Insert({"Psuedophedrine", "345,223", "True"});
 	hp.Insert({"Tylenol", "452,334", "False"});
-  db.InsertTable(hp);
+	db.InsertTable(hp);
 	db.Print();
-  db.SaveData();
+	db.SaveData();
 
 	std::vector< std::vector < std::string > > r = hp.GetRowsWithValue("RequiresID", "True");
 	for (int i = 0; i < r.size(); i++){
 		PrintVector(r[i]);
 		std::cout << std::endl;
 	}
-  
-  Database datab("test.txt");
-  datab.LoadTables();
 
-  Table tb = datab.GetTable("Patients");
+	Database datab("test.txt");
+	datab.LoadTables();
+
+	Table tb = datab.GetTable("Patients");
 	tb.Print();
-  PrintVector(tb.GetColumn("Name"));
-  PrintVector(tb.GetRow(0));
+	PrintVector(tb.GetColumn("Name"));
+	PrintVector(tb.GetRow(0));
 }
