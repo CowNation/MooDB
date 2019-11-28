f39f0fe0d0761a12b257f486bc702aefe2adc78a
Joan Andrés <andres6936@live.com>
2019-11-28 18:36:01 -0500

    Reformat Code

--- Data/CowConfig.hpp
+++ Data/CowConfig.hpp
@@ -56,128 +56,188 @@
 #include <algorithm>
 #include <sstream>
 
-class CowConfig {
+class CowConfig
+{
 private:
-	std::ofstream WriteConfig;
-	std::fstream ReadConfig;
-	bool FirstLine = true, FirstRead = true;
-	std::string FileName;
-	std::vector< std::string >Lines;
-  	///////////////////
-  	void RemoveSubStr(std::string substr, std::string& str){
-    	size_t pos = std::string::npos;
-		
-		while ((pos = str.find(substr)) != std::string::npos)
-			str.erase(pos, substr.length());
-  	}
-  	///////////////////
-  	void ReadAllLines(){
-    	std::string str;
-    	while (std::getline(ReadConfig, str))
-      		Lines.push_back(str);
-  	}
-  	///////////////////
-  	int FindElement(std::string Section, std::string offsetText){
-		Section = "[" + Section + "]";
-		bool SectionFound;
-		for (int i = 0; i < Lines.size(); i++){
-			if (Lines[i] == Section || Section == "[]")
-				SectionFound = true;
-			else if (SectionFound && Lines[i].find("[") != std::string::npos && Lines[i].find("]") != std::string::npos)
-				break;
-			if (SectionFound && Lines[i].find(offsetText) != std::string::npos)
-				return i;
-		}
-		return -1;
-  	}
-  	///////////////////
-	int pRead(std::string Section, std::string offsetText){
-		if (FirstRead)
-      		ReadAllLines();
-    	return FindElement(Section, offsetText);
-	}
+    std::ofstream WriteConfig;
+    std::fstream ReadConfig;
+    bool FirstLine = true, FirstRead = true;
+    std::string FileName;
+    std::vector <std::string> Lines;
+
+    ///////////////////
+    void RemoveSubStr( std::string substr, std::string &str )
+    {
+        size_t pos = std::string::npos;
+
+        while (( pos = str.find( substr )) != std::string::npos )
+        {
+            str.erase( pos, substr.length( ));
+        }
+    }
+
+    ///////////////////
+    void ReadAllLines( )
+    {
+        std::string str;
+        while ( std::getline( ReadConfig, str ))
+        {
+            Lines.push_back( str );
+        }
+    }
+
+    ///////////////////
+    int FindElement( std::string Section, std::string offsetText )
+    {
+        Section = "[" + Section + "]";
+        bool SectionFound;
+        for ( int i = 0; i < Lines.size( ); i++ )
+        {
+            if ( Lines[ i ] == Section || Section == "[]" )
+            {
+                SectionFound = true;
+            }
+            else if ( SectionFound && Lines[ i ].find( "[" ) != std::string::npos &&
+                      Lines[ i ].find( "]" ) != std::string::npos )
+            {
+                break;
+            }
+            if ( SectionFound && Lines[ i ].find( offsetText ) != std::string::npos )
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    ///////////////////
+    int pRead( std::string Section, std::string offsetText )
+    {
+        if ( FirstRead )
+        {
+            ReadAllLines( );
+        }
+        return FindElement( Section, offsetText );
+    }
+
 public:
-	CowConfig() {}
-	CowConfig(std::string fileName){
-    	OpenFile(fileName);
-	}
-	~CowConfig() {
-		if (ReadConfig.is_open())
-			ReadConfig.close();
-		if (WriteConfig.is_open())
-			WriteConfig.close();
-	}
-	///////////////////
-	bool OpenFile(std::string fileName) {
-		FileName = fileName;
+    CowConfig( )
+    { }
 
-		ReadConfig.open(fileName);
-		return (ReadConfig.is_open());
-	} // Will open file with specified filename and will return whether the file is open
-	//////////////////
-	std::vector< std::string > GetLines(){
-		std::string str;
-		std::vector< std::string > ret;
-    	while (std::getline(ReadConfig, str))
-      		ret.push_back(str);
-		return ret;
-	}
-	//////////////////
-	void WriteLine(std::string offsetText, std::string valToWrite) {
-		if (ReadConfig.is_open())
-			ReadConfig.close(); //Close fstream file before writing to the file
+    CowConfig( std::string fileName )
+    {
+        OpenFile( fileName );
+    }
 
-		if (FirstLine) {
-			WriteConfig.open(FileName);
-			FirstLine = false;
-		}
+    ~CowConfig( )
+    {
+        if ( ReadConfig.is_open( ))
+        {
+            ReadConfig.close( );
+        }
+        if ( WriteConfig.is_open( ))
+        {
+            WriteConfig.close( );
+        }
+    }
 
-		WriteConfig << offsetText << valToWrite << std::endl;
+    ///////////////////
+    bool OpenFile( std::string fileName )
+    {
+        FileName = fileName;
 
-		ReadConfig.open(FileName);
-	}
-	template <class c>
-	void WriteLine(std::string offsetText, c valToWrite) {
-		if (ReadConfig.is_open())
-			ReadConfig.close(); //Close fstream file before writing to the file
+        ReadConfig.open( fileName );
+        return ( ReadConfig.is_open( ));
+    } // Will open file with specified filename and will return whether the file is open
+    //////////////////
+    std::vector <std::string> GetLines( )
+    {
+        std::string str;
+        std::vector <std::string> ret;
+        while ( std::getline( ReadConfig, str ))
+        {
+            ret.push_back( str );
+        }
+        return ret;
+    }
 
-		if (FirstLine) {
-			WriteConfig.open(FileName);
-			FirstLine = false;
-		}
+    //////////////////
+    void WriteLine( std::string offsetText, std::string valToWrite )
+    {
+        if ( ReadConfig.is_open( ))
+        {
+            ReadConfig.close( );
+        } //Close fstream file before writing to the file
 
-		WriteConfig << offsetText << std::to_string(valToWrite) << std::endl;
+        if ( FirstLine )
+        {
+            WriteConfig.open( FileName );
+            FirstLine = false;
+        }
 
-		ReadConfig.open(FileName);
-	}
-  	void Section(std::string SectionText) {
-		SectionText = "[" + SectionText + "]";
-    	WriteLine("", SectionText);
-  	}
-	//////////////////
-	template <class T>
-	T Read(std::string Section, std::string offsetText){
-		int _Read = pRead(Section, offsetText);
-    		if (_Read == -1)
-      			return 0;
-    		else{
-      			try{
-        			std::string temp = Lines[_Read];
-        			RemoveSubStr(offsetText, temp);
-					T ret;
-					std::istringstream(temp) >> ret;
-					return ret;
-      			}
-      			catch (...){
-        			return T();
-      			}
-    		}
-	}
-	//////////////////
-	void ClearFile() {
-		std::ofstream Clear;
-		Clear.open(FileName);
-		Clear.close();
-	}
+        WriteConfig << offsetText << valToWrite << std::endl;
+
+        ReadConfig.open( FileName );
+    }
+
+    template <class c>
+    void WriteLine( std::string offsetText, c valToWrite )
+    {
+        if ( ReadConfig.is_open( ))
+        {
+            ReadConfig.close( );
+        } //Close fstream file before writing to the file
+
+        if ( FirstLine )
+        {
+            WriteConfig.open( FileName );
+            FirstLine = false;
+        }
+
+        WriteConfig << offsetText << std::to_string( valToWrite ) << std::endl;
+
+        ReadConfig.open( FileName );
+    }
+
+    void Section( std::string SectionText )
+    {
+        SectionText = "[" + SectionText + "]";
+        WriteLine( "", SectionText );
+    }
+
+    //////////////////
+    template <class T>
+    T Read( std::string Section, std::string offsetText )
+    {
+        int _Read = pRead( Section, offsetText );
+        if ( _Read == -1 )
+        {
+            return 0;
+        }
+        else
+        {
+            try
+            {
+                std::string temp = Lines[ _Read ];
+                RemoveSubStr( offsetText, temp );
+                T ret;
+                std::istringstream( temp ) >> ret;
+                return ret;
+            }
+            catch ( ... )
+            {
+                return T( );
+            }
+        }
+    }
+
+    //////////////////
+    void ClearFile( )
+    {
+        std::ofstream Clear;
+        Clear.open( FileName );
+        Clear.close( );
+    }
 };
+
 #endif
--- Data/MooTable.hpp
+++ Data/MooTable.hpp
@@ -4,131 +4,185 @@
 
 class Database;
 
-class Table{
+class Table
+{
 private:
-	bool StringVectorHas(std::vector< std::string > vec, std::string substr){
-	for (int i = 0; i < vec.size(); i++)
-	if (vec[i].find(substr) != std::string::npos)
-	return true;
-	return false;
-	}
+    bool StringVectorHas( std::vector <std::string> vec, std::string substr )
+    {
+        for ( int i = 0; i < vec.size( ); i++ )
+        {
+            if ( vec[ i ].find( substr ) != std::string::npos )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 public:
-	std::vector< std::string > Columns; // A horizonal vector containing the number of columns and their names
-	std::vector< std::vector< std::string > > Data; // A vertical vector (list) of horizontal vectors containing string data
+    std::vector <std::string> Columns; // A horizonal vector containing the number of columns and their names
+    std::vector <std::vector <std::string> > Data; // A vertical vector (list) of horizontal vectors containing string data
 
-	Database* parentDB = nullptr;
-	std::string Name;
+    Database *parentDB = nullptr;
+    std::string Name;
 
-	void RefreshFile();
+    void RefreshFile( );
 
-	void Insert(std::vector< std::string > data) {
-	if (data.size() != Columns.size())
-	throw "Inserted data does not match columns";
-	if (StringVectorHas(data, "{") || StringVectorHas(data, "}") || StringVectorHas(data, ""))
-	throw "Inserted data contains a forbidden character";
-	Data.push_back(data);
-	}
+    void Insert( std::vector <std::string> data )
+    {
+        if ( data.size( ) != Columns.size( ))
+        {
+            throw "Inserted data does not match columns";
+        }
+        if ( StringVectorHas( data, "{" ) || StringVectorHas( data, "}" ) || StringVectorHas( data, "" ))
+        {
+            throw "Inserted data contains a forbidden character";
+        }
+        Data.push_back( data );
+    }
 
-	void Print(){
-		std::vector< int > ColumnOffsets;
+    void Print( )
+    {
+        std::vector <int> ColumnOffsets;
 
-		for (int i = 0; i < Columns.size(); i++)
-			ColumnOffsets.push_back(-1);
+        for ( int i = 0; i < Columns.size( ); i++ )
+        {
+            ColumnOffsets.push_back( -1 );
+        }
 
-		for (int i = 0; i < Data.size(); i++){
-			for (int j = 0; j < Data[i].size(); j++){
-				if ((Data[i][j].length() - Columns[j].length() > ColumnOffsets[j] || ColumnOffsets[j] == -1) && Data[i][j].length() > Columns[j].length())
-					ColumnOffsets[j] = Data[i][j].length() - Columns[j].length();
-			} // we are looping horizontally
-		} // we are looping vertically
+        for ( int i = 0; i < Data.size( ); i++ )
+        {
+            for ( int j = 0; j < Data[ i ].size( ); j++ )
+            {
+                if (( Data[ i ][ j ].length( ) - Columns[ j ].length( ) > ColumnOffsets[ j ] ||
+                      ColumnOffsets[ j ] == -1 ) && Data[ i ][ j ].length( ) > Columns[ j ].length( ))
+                {
+                    ColumnOffsets[ j ] = Data[ i ][ j ].length( ) - Columns[ j ].length( );
+                }
+            } // we are looping horizontally
+        } // we are looping vertically
 
-		std::cout << std::endl << Name << ":" << std::endl;
+        std::cout << std::endl << Name << ":" << std::endl;
 
-		std::string Headers;
-		std::string Seperator;
+        std::string Headers;
+        std::string Seperator;
 
-		Headers += "| ";
-		for (int i = 0; i < Columns.size(); i++){
-			Headers += Columns[i];
-			if (ColumnOffsets[i] != -1){
-				for (int j = 0; j < ColumnOffsets[i]; j++)
-					Headers += " ";
-			}
-			Headers += " | ";
-		}
+        Headers += "| ";
+        for ( int i = 0; i < Columns.size( ); i++ )
+        {
+            Headers += Columns[ i ];
+            if ( ColumnOffsets[ i ] != -1 )
+            {
+                for ( int j = 0; j < ColumnOffsets[ i ]; j++ )
+                {
+                    Headers += " ";
+                }
+            }
+            Headers += " | ";
+        }
 
-		Seperator += "+";
-		for (int i = 0; i < Headers.length() - 3; i++){
-			if (Headers.at(i + 1) == '|')
-				Seperator += "+";
-			else
-				Seperator += "-";
-		}
+        Seperator += "+";
+        for ( int i = 0; i < Headers.length( ) - 3; i++ )
+        {
+            if ( Headers.at( i + 1 ) == '|' )
+            {
+                Seperator += "+";
+            }
+            else
+            {
+                Seperator += "-";
+            }
+        }
 
-		Seperator += "+";
+        Seperator += "+";
 
-		std::cout << Seperator << "\n" << Headers << "\n" << Seperator;
+        std::cout << Seperator << "\n" << Headers << "\n" << Seperator;
 
-		for (int i = 0; i < Data.size(); i++){
-			std::cout << "\n| ";
-			for (int j = 0; j < Data[i].size(); j++){
-				std::cout << Data[i][j];
+        for ( int i = 0; i < Data.size( ); i++ )
+        {
+            std::cout << "\n| ";
+            for ( int j = 0; j < Data[ i ].size( ); j++ )
+            {
+                std::cout << Data[ i ][ j ];
 
-				if (ColumnOffsets[j] != -1){
-					if (Data[i][j].length() < (ColumnOffsets[j] + Columns[j].length())){
-						for (int p = 0; p < (ColumnOffsets[j] + Columns[j].length()) - Data[i][j].length(); p++)
-						std::cout << " ";
-					}
-				}
-				else{
-					for (int p = 0; p < Columns[j].length() - Data[i][j].length(); p++)
-						std::cout << " ";
-				}
+                if ( ColumnOffsets[ j ] != -1 )
+                {
+                    if ( Data[ i ][ j ].length( ) < ( ColumnOffsets[ j ] + Columns[ j ].length( )))
+                    {
+                        for ( int p = 0;
+                              p < ( ColumnOffsets[ j ] + Columns[ j ].length( )) - Data[ i ][ j ].length( ); p++ )
+                        {
+                            std::cout << " ";
+                        }
+                    }
+                }
+                else
+                {
+                    for ( int p = 0; p < Columns[ j ].length( ) - Data[ i ][ j ].length( ); p++ )
+                    {
+                        std::cout << " ";
+                    }
+                }
 
-				std::cout << " | ";
-			} // we are looping horizontally
-		} // we are looping vertically
+                std::cout << " | ";
+            } // we are looping horizontally
+        } // we are looping vertically
 
-		std::cout << std::endl << Seperator << std::endl;
-	}
+        std::cout << std::endl << Seperator << std::endl;
+    }
 
-	Table(std::string tableName, std::vector < std::string > columns){
-		Name = tableName;
-		Columns = columns;
-	}
+    Table( std::string tableName, std::vector <std::string> columns )
+    {
+        Name = tableName;
+        Columns = columns;
+    }
 
-	std::vector< std::string > GetRow(int Index){
-		if (Index > Data.size() || Index < 0)
-			throw "Row at index " + std::to_string(Index) + " is out of bounds";
-		return Data[Index];
-	}
+    std::vector <std::string> GetRow( int Index )
+    {
+        if ( Index > Data.size( ) || Index < 0 )
+        {
+            throw "Row at index " + std::to_string( Index ) + " is out of bounds";
+        }
+        return Data[ Index ];
+    }
 
-	std::vector< std::vector < std::string > > GetRowsWithValue(std::string nColumn, std::string ColumnValue){
-		std::vector< std::vector < std::string > > ret;
-		std::vector< std::string > vColumn = GetColumn(nColumn);
-		for (int i = 0; i < vColumn.size(); i++){
-			if (vColumn[i] == ColumnValue)
-				ret.push_back(GetRow(i));
-		}
-		return ret;
-	} // will return a vector of vectors of strings where that row's column value equals the 'ColumnValue'
+    std::vector <std::vector <std::string> > GetRowsWithValue( std::string nColumn, std::string ColumnValue )
+    {
+        std::vector <std::vector <std::string> > ret;
+        std::vector <std::string> vColumn = GetColumn( nColumn );
+        for ( int i = 0; i < vColumn.size( ); i++ )
+        {
+            if ( vColumn[ i ] == ColumnValue )
+            {
+                ret.push_back( GetRow( i ));
+            }
+        }
+        return ret;
+    } // will return a vector of vectors of strings where that row's column value equals the 'ColumnValue'
 
-	std::vector< std::string > GetColumn(std::string ColumnName){
-		std::vector< std::string > ret;
-		int ColumnIndex = -1;
-		for (int i = 0; i < Columns.size(); i++){
-			if (Columns[i] == ColumnName){
-				ColumnIndex = i;
-				break;
-			}
-		}
+    std::vector <std::string> GetColumn( std::string ColumnName )
+    {
+        std::vector <std::string> ret;
+        int ColumnIndex = -1;
+        for ( int i = 0; i < Columns.size( ); i++ )
+        {
+            if ( Columns[ i ] == ColumnName )
+            {
+                ColumnIndex = i;
+                break;
+            }
+        }
 
-		if (ColumnIndex == -1)
-			throw "Could not find column '" + ColumnName + "'";
-		
-		for (int i = 0; i < Data.size(); i++)
-			ret.push_back(Data[i][ColumnIndex]);
+        if ( ColumnIndex == -1 )
+        {
+            throw "Could not find column '" + ColumnName + "'";
+        }
 
-		return ret;
-	}
+        for ( int i = 0; i < Data.size( ); i++ )
+        {
+            ret.push_back( Data[ i ][ ColumnIndex ] );
+        }
+
+        return ret;
+    }
 };
