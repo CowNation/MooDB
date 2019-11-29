2d1a66524c30f17e107957116c913a6ee3ad118b
Joan Andrés <andres6936@live.com>
2019-11-28 19:53:00 -0500

    Use of const and references

--- Data/MooTable.hpp
+++ Data/MooTable.hpp
@@ -141,19 +141,23 @@ public:
         Columns = columns;
     }
 
-    std::vector <std::string> GetRow( int Index )
+    std::vector <std::string> GetRow( const int Index )
     {
         if ( Index > Data.size( ) || Index < 0 )
         {
             throw "Row at index " + std::to_string( Index ) + " is out of bounds";
         }
+
         return Data[ Index ];
     }
 
-    std::vector <std::vector <std::string> > GetRowsWithValue( std::string nColumn, std::string ColumnValue )
+    std::vector <std::vector <std::string> >
+    GetRowsWithValue( const std::string &nColumn, const std::string &ColumnValue )
     {
         std::vector <std::vector <std::string> > ret;
+
         std::vector <std::string> vColumn = GetColumn( nColumn );
+
         for ( int i = 0; i < vColumn.size( ); i++ )
         {
             if ( vColumn[ i ] == ColumnValue )
@@ -161,13 +165,16 @@ public:
                 ret.push_back( GetRow( i ));
             }
         }
+
         return ret;
     } // will return a vector of vectors of strings where that row's column value equals the 'ColumnValue'
 
-    std::vector <std::string> GetColumn( std::string ColumnName )
+    std::vector <std::string> GetColumn( const std::string &ColumnName )
     {
         std::vector <std::string> ret;
+
         int ColumnIndex = -1;
+
         for ( int i = 0; i < Columns.size( ); i++ )
         {
             if ( Columns[ i ] == ColumnName )
@@ -182,7 +189,7 @@ public:
             throw "Could not find column '" + ColumnName + "'";
         }
 
-        for (auto & i : Data)
+        for ( std::vector <std::string> &i : Data )
         {
             ret.push_back( i[ ColumnIndex ] );
         }
