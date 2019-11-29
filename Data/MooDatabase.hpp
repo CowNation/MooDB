9020bfe96214d3f724f837e7c562407fcc5f6850
Joan Andrés <andres6936@live.com>
2019-11-28 19:14:52 -0500

    Use of based for-loop

--- Data/MooDatabase.hpp
+++ Data/MooDatabase.hpp
@@ -6,40 +6,48 @@
 
 class Database
 {
+
 private:
-    std::string FindStrBetween( std::string &str, char firstL, char lastL, bool Remove )
+
+    static std::string FindStrBetween( std::string &str, char firstL, char lastL, bool Remove )
     {
         std::string ret;
+
         bool between = false;
-        for ( int i = 0; i < str.length( ); i++ )
+
+        for (char i : str)
         {
-            if ( between && str[ i ] == lastL )
+            if ( between && i == lastL )
             {
                 break;
             }
 
-            if ( str[ i ] == firstL && between == false )
+            if ( i == firstL && !between )
             {
                 between = true;
             }
             else if ( between )
             {
-                ret += str[ i ];
+                ret += i;
             }
         }
+
         str.erase( 0, ret.length( ) + 2 );
+
         return ret;
     }
 
-    std::vector <std::string> UnseperateCommas( std::string str )
+    static std::vector <std::string> UnseperateCommas( std::string str )
     {
         std::vector <std::string> ret;
+
         std::string temp;
-        for ( int i = 0; i < str.length( ); i++ )
+
+        for (char i : str)
         {
-            if ( str[ i ] != '' )
+            if ( i != '' )
             {
-                temp += str[ i ];
+                temp += i;
             }
             else
             {
@@ -47,23 +55,26 @@ private:
                 temp = "";
             }
         }
+
         return ret;
     }
 
     CowConfig cfg;
+
 public:
+
     std::vector <Table> Tables;
 
-    Database( std::string filePath )
+    explicit Database( std::string filePath )
     {
         cfg.OpenFile( filePath );
     }
 
     void Print( )
     {
-        for ( int i = 0; i < Tables.size( ); i++ )
+        for (Table &Table : Tables)
         {
-            Tables[ i ].Print( );
+            Table.Print( );
         }
     }
 
@@ -79,26 +90,33 @@ public:
         Saved data format:
         1: {Table1Name{Column1Name,Column2Name}{OneOne,OneTwo}{TwoOne,TwoTwo}}
         */
-        for ( int i = 0; i < Tables.size( ); i++ )
+        for (Table &Table : Tables)
         {
-            std::string tableLine = "{" + Tables[ i ].Name + "}{";
-            for ( int p = 0; p < Tables[ i ].Columns.size( ); p++ )
+            std::string tableLine = "{" + Table.Name + "}{";
+
+            for (const std::string &Column : Table.Columns)
             {
-                tableLine += Tables[ i ].Columns[ p ] + "";
+                tableLine += Column + "";
             }
+
             tableLine += "}{";
-            for ( int p = 0; p < Tables[ i ].Data.size( ); p++ )
+
+            for ( int p = 0; p < Table.Data.size( ); p++ )
             {
-                for ( int q = 0; q < Tables[ i ].Data[ p ].size( ); q++ )
+                for (const std::string &q : Table.Data[ p ])
                 {
-                    tableLine += Tables[ i ].Data[ p ][ q ] + "";
+                    tableLine += q + "";
                 } // looping horizontally
+
                 tableLine += "}";
-                if ( p != Tables[ i ].Data.size( ) - 1 )
+
+                if ( p != Table.Data.size( ) - 1 )
                 {
                     tableLine += "{";
                 }
+
             }// looping vertically
+
             cfg.WriteLine( "", tableLine );
         }
     }
@@ -106,13 +124,16 @@ public:
     void LoadTables( )
     {
         std::vector <std::string> Lines = cfg.GetLines( );
-        for ( int i = 0; i < Lines.size( ); i++ )
+
+        for (std::string & Line : Lines)
         {
-            std::string Name = FindStrBetween( Lines[ i ], '{', '}', true );
-            Table tab( Name, UnseperateCommas( FindStrBetween( Lines[ i ], '{', '}', true )));
-            while ( Lines[ i ].length( ) > 0 )
+            std::string Name = FindStrBetween( Line, '{', '}', true );
+
+            Table tab( Name, UnseperateCommas( FindStrBetween( Line, '{', '}', true )));
+
+            while ( Line.length( ) > 0 )
             {
-                std::vector <std::string> vc = UnseperateCommas( FindStrBetween( Lines[ i ], '{', '}', true ));
+                std::vector <std::string> vc = UnseperateCommas( FindStrBetween( Line, '{', '}', true ));
                 tab.Insert( vc );
             }
 
@@ -122,13 +143,14 @@ public:
 
     Table &GetTable( std::string TableName )
     {
-        for ( int i = 0; i < Tables.size( ); i++ )
+        for (Table &Table : Tables)
         {
-            if ( Tables[ i ].Name == TableName )
+            if ( Table.Name == TableName )
             {
-                return Tables[ i ];
+                return Table;
             }
         }
+
         throw "Could not find table '" + TableName + "'";
     }
 };
