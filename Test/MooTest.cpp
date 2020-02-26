a9dd9c8d25053da316e53a97975b216767757c59
Joan Andrés <andres6936@live.com>
2020-02-26 13:00:07 -0500

    Added Test

--- Test/MooTest.cpp
+++ Test/MooTest.cpp
@@ -11,11 +11,28 @@ using namespace Moo;
 // Anonymous namespace
 namespace
 {
-	TEST(MooTable, InsertDateAndSizeIncreaseInOne)
+	TEST(MooTable, Empty)
+	{
+		Table tab({ "Name", "Gender", "Age", "DOB", "Height", "Weight" });
+
+		// The table always is empty to be created
+		EXPECT_EQ(tab.SizeInRows(), 0);
+	}
+
+	TEST(MooTable, SizeIncrease)
 	{
 		Table tab({ "Name", "Gender", "Age", "DOB", "Height", "Weight" });
 		tab.Insert({ "Funanya Radomir", "Female", "3", "6/1/2015", "3 ft 1 in", "29.5 lbs" });
 
-		EXPECT_TRUE(1 == tab.SizeInRows());
+		// The size of table increase in one
+		EXPECT_EQ(tab.SizeInRows(), 1);
 	}
+}
+
+int main()
+{
+	// Necessary for avoid warning for part of Google
+	testing::InitGoogleTest();
+	// Execute all the Test
+	return RUN_ALL_TESTS();
 }
