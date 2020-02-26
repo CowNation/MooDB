a97e4c94d3118ff15f825a3ac7a63fa6bb4b4c7a
Joan Andrés <andres6936@live.com>
2020-02-26 09:54:54 -0500

    Added Support to Google Test

---
+++ Test/MooDBTest.cpp
@@ -0,0 +1,17 @@
+// Relax, you can deleted this lines
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include <gtest/gtest.h>
+
+#include "MooDb.hpp"
+
+// Anonymous namespace
+namespace
+{
+	TEST(MooDB, Insert)
+	{
+		EXPECT_TRUE(true);
+		EXPECT_TRUE(false);
+	}
+}
